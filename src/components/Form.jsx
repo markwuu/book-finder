39f4d7142aadf13ff433bb10aa60ce2dfa391452
Mark W <markwuu@gmail.com>
2019-01-24 20:53:31 -1000

    Change button styles and removed placeholder state

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import styled from "styled-components";
+import magnifier from "../magnifier.svg";
 
 const SearchContainer = styled.div`
   display: flex;
@@ -30,48 +31,11 @@ const Input = styled.input`
 `;
 
 const Button = styled.button`
-  font-size: 30px;
-  border-bottom-right-radius: 3px;
-  border-top-right-radius: 3px;
-  display: inline-block;
-  vertical-align: middle;
-  -webkit-transform: perspective(1px) translateZ(0);
-  transform: perspective(1px) translateZ(0);
-  box-shadow: 0 0 1px rgba(0, 0, 0, 0);
-  position: relative;
-  -webkit-transition-property: #5ab9ea;
-  transition-property: #5ab9ea;
-  -webkit-transition-duration: 0.3s;
-
-  &:before {
-    content: "";
-    position: absolute;
-    z-index: -1;
-    top: 0;
-    left: 0;
-    right: 0;
-    bottom: 0;
-    background: #202832;
-    -webkit-transform: scaleX(0);
-    transform: scaleX(0);
-    -webkit-transform-origin: 0 50%;
-    transform-origin: 0 50%;
-    -webkit-transition-property: transform;
-    transition-property: transform;
-    -webkit-transition-duration: 0.3s;
-    transition-duration: 0.4s;
-    -webkit-transition-timing-function: ease-out;
-    transition-timing-function: ease-out;
-  }
-  &:hover {
-    color: white;
-    &:before {
-      -webkit-transform: scaleX(1);
-      transform: scaleX(1);
-    }
-  }
+  background: url(${magnifier}) no-repeat center;
+  border-bottom-left-radius: 0;
+  border-top-left-radius: 0;
+  font-size: 20px;
 `;
-
 class Form extends Component {
   handleFormSubmit = e => {
     e.preventDefault();
@@ -87,7 +51,7 @@ class Form extends Component {
             onChange={this.props.handleInput}
             required
           />{" "}
-          <Button type="submit">Search</Button>
+          <Button type="submit" />
         </BookForm>
       </SearchContainer>
     );
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -25,9 +25,12 @@ const Nav = () => {
   return (
     <React.Fragment>
       <NavBar>
-        <a href="https://github.com/markwuu">Github</a>
+        <a href="https://github.com/markwuu/book-finder">Github</a>
         <a href="https://reactjs.org/">React</a>
         <a href="https://heroku.com/">Heroku</a>
+        <a href="https://developers.google.com/books/">Google Books API</a>
+        <a href="https://www.styled-components.com/">styled-components</a>
+        <a href="https://github.com/axios/axios">axios</a>
       </NavBar>
     </React.Fragment>
   );
