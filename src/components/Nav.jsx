d1382f666effbe4d4bc58b389816338834ba9303
Mark W <markwuu@gmail.com>
2019-02-05 20:18:58 -1000

    Abstract out links to an array to DRY up code

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -25,16 +25,20 @@ const NavBar = styled.div`
 `;
 
 const Nav = () => {
+  const links = [
+    { url: "https://github.com/markwuu/book-finder", name: "Github" },
+    { url: "https://reactjs.org/", name: "React" },
+    { url: "https://heroku.com/", name: "Heroku" },
+    { url: "https://developers.google.com/books/", name: "Google Books API" },
+    { url: "https://www.styled-components.com/", name: "styled-components" },
+    { url: "https://github.com/axios/axios", name: "axios" }
+  ];
   return (
     <React.Fragment>
       <NavBar>
-        <a href="https://github.com/markwuu/book-finder">Github</a>
-        <a href="https://reactjs.org/">React</a>
-        <a href="https://heroku.com/">Heroku</a>
-        <a href="https://developers.google.com/books/">Google Books API</a>
-        <a href="https://www.styled-components.com/">styled-components</a>
-        <a href="https://github.com/axios/axios">axios</a>
-        <a href="https://www.neopets.com">Neopets</a>
+        {links.map(link => {
+          return <a href={link.url}>{link.name}</a>;
+        })}
       </NavBar>
     </React.Fragment>
   );
