828e7b405fd9843c1224e5b0413f0f2927e600ea
Mark W <markwuu@gmail.com>
2019-02-05 20:22:14 -1000

    Separate the nav bar styles into the UI folder

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,28 +1,5 @@
 import React from "react";
-import styled from "styled-components";
-
-const NavBar = styled.div`
-  background-color: #1f2833;
-  display: flex;
-  overflow: hidden;
-  position: fixed;
-  top: 0;
-  width: 100%;
-  z-index: 1;
-  a {
-    color: white;
-    display: block;
-    float: left;
-    font-size: 2vh;
-    padding: 17px;
-    text-align: center;
-    text-decoration: none;
-
-    &:hover {
-      background-color: #1f2850;
-    }
-  }
-`;
+import NavBar from "./UI/NavBar";
 
 const Nav = () => {
   const links = [
