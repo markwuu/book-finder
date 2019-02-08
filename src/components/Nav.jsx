75e4ec8581949a3e319c193a29e8e911b7ccc6e8
Mark W <markwuu@gmail.com>
2019-02-07 20:02:29 -1000

    Revert "Separate the nav bar styles into the UI folder"
    This reverts commit 828e7b405fd9843c1224e5b0413f0f2927e600ea.

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,5 +1,28 @@
 import React from "react";
-import NavBar from "./UI/NavBar";
+import styled from "styled-components";
+
+const NavBar = styled.div`
+  background-color: #1f2833;
+  display: flex;
+  overflow: hidden;
+  position: fixed;
+  top: 0;
+  width: 100%;
+  z-index: 1;
+  a {
+    color: white;
+    display: block;
+    float: left;
+    font-size: 2vh;
+    padding: 17px;
+    text-align: center;
+    text-decoration: none;
+
+    &:hover {
+      background-color: #1f2850;
+    }
+  }
+`;
 
 const Nav = () => {
   const links = [
